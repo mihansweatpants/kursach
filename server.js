0cb62efd8165658af0941c4c483316bc4bf6673c
Миша Барсегян <32730665+mihansweatpants@users.noreply.github.com>
2018-05-23 13:05:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// импорт модуля http и объекта app, сгенерированного с помощью Express
 const http = require('http');
 const app = require('./app');
 
-// создание сервера
 const server = http.createServer(app);
 
-// определение порта, на котором будет работать API 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 
